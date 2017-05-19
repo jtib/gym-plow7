50237d5f12b8a8155dd1b06679f15a67164f59d8
jtib <jtib@ntymail.com>
2017-05-19 10:33:43 -0400

    Essential files for a gym env

---
+++ gym-plow7/__init__.py
@@ -0,0 +1,10 @@
+from gym.envs.registration import register
+
+register(
+        id='plow7-v0',
+        entry_point='gym_plow7.envs:Plow7Env',
+        )
+register(
+        id='plow7_extrahard-v0',
+        entry_point='gym_plow7.envs:Plow7ExtraHardEnv',
+        )
---
+++ gym-plow7/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_plow7.envs.plow7_env import Plow7Env
+from gym_plow7.envs.plow7_extrahard_env import Plow7ExtraHardEnv
