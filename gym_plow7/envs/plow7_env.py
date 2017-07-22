518cb978aeb020a7503fdf3d3ea5325338867391
jtib <jtib@ntymail.com>
2017-07-22 01:51:18 -0400

    integrated PLOW challenge solution by Dentinger, Harilela & Belhaddad

--- gym_plow7/envs/plow7_env.py
+++ gym_plow7/envs/plow7_env.py
@@ -6,6 +6,14 @@ from gym_argos3.envs.argos3_env import Argos3Env, logger
 
 import numpy as np
 
+def getUpdatedReward(reward, multiplier):
+    ret = reward
+    if ret >= 0:
+        ret *= multiplier
+    else:
+        ret *= -multiplier
+    return ret
+
 class Plow7Env(Argos3Env):
     """
     OpenAI gym environment for a specific crossroads setting in ARGoS3.
@@ -25,12 +33,12 @@ class Plow7Env(Argos3Env):
         self.observation_space = spaces.Box(-np.ones([self.obs_len]), np.ones([self.obs_len])) # will have to normalize observations
         self.av_speeds = np.zeros(number_footbots)
         self.data_type = dt
-        self.all_speeds = np.zeros((self.t_max, self.nbFb))
+        self.min_speed = min_speed
+        self.max_speed = max_speed
+        self.timesteps = 0
         super().setParams(number_footbots, min_speed, max_speed, dt)
 
     def process_raw_state(self, raw_state):
-        self.all_speeds[self.t] = raw_state[48*self.nbFb:49*self.nbFb]
-
         return raw_state
 
     def _reset(self):
@@ -44,16 +52,12 @@ class Plow7Env(Argos3Env):
 
     def _step(self, action):
         action = self.action_space.low + (action+np.ones(self.nbFb))*(self.action_space.high[0]-self.action_space.low[0])/2
-        for a in action:
-            if a < self.action_space.low[0]:
-                a = self.action_space.low[0]
-            if a > self.action_space.high[0]:
-                a = self.action_space.high
         self.send(action)
         state, frame = self.receive()
         state = self.process_raw_state(state)
 
         speeds = state[48*self.nbFb:49*self.nbFb] # all fb positions
+        print(f"Speeds: {speeds}")
         dist_dep = state[49*self.nbFb:] # distance from init. pos
         proximities = state[:48*self.nbFb] # all fb proxim. readings
         proximities = np.array(proximities)
@@ -62,16 +66,45 @@ class Plow7Env(Argos3Env):
             print(f"Episode: {self.t}  Speeds: {speeds}")
             print(f"Distances: {dist_dep}")
             print(f"Proximities: {proximities}")
-        prox = np.sum(proximities, axis=1)
         self.av_speeds = (self.av_speeds*self.t + speeds)/(self.t+1)
 
-        done = all(dist_dep > 7)
-        reward = sum(self.av_speeds - 1000*sum(prox[:,0]>.95) + 5*dist_dep)
-        if done:
-            reward += 1000
+        # Dentinger & team:
+        # better to use the "distance to collision"
+        # rather than plain distance
+        # because the two footbots that go in the
+        # same direction will stay close to each other
+        # but that shouldn't cause a loss in reward.
+        #
+        # Other improvement: data crunching
+        # As it was: data of ALL footbots crushed at once
+        # Much better information by taking
+        # a reward per action per footbot
+        # rather than for all footbots together.
+
+        reward = 1
+
+        for fbProx in proximities:
+            for prox in fbProx:
+                reward -= 10 * prox[0]**2 # The closer you get, the less I reward you
+
+        for speed in speeds:
+            if speed < self.min_speed or speed > self.max_speed:
+                reward -= 5
+            else:
+                # Encouraged to go as fast as possible
+                reward += 3 * (speed - self.min_speed) / (self.max_speed - self.min_speed)
+
+        self.timesteps += 1
+        done = (self.timesteps >= 300)
+
+        # done = all(dist_dep > 7) # This is broken ; it always returns True after a while
+                                   # Not sure how it is calcualted.
+        # if done:
+        #     reward += 1000
+        #     print("DONE")
 
         self.t += 1
-
+        print("reward: ", reward)
         return state, reward, done, {}
 
     def _render(self, mode='human', close=False):
